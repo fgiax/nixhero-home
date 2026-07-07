2510f12e57cebcae78d8c8376d8e0c2e38064c89
zuger <fgiax02@gmail.com>
2026-07-07 13:20:00 +0000

    smth git

--- git/default.nix
+++ git/default.nix
@@ -3,7 +3,37 @@
 {
   programs.git = {
     enable = true;
-    userName = "zuger";
-    userEmail = "fgiax02@gmail.com";
+    
+    settings = {
+      user = {
+        name = "zuger";
+        email = "fgiax02@gmail.com";
+      };
+      
+      init = {
+        defaultBranch = "main";
+      };
+      
+      core = {
+        editor = "nvim";
+        autocrlf = "input";
+      };
+      
+      push = {
+        default = "simple";
+      };
+      
+      alias = {
+        co = "checkout";
+        br = "branch";
+        st = "status";
+        ci = "commit";
+      };
+      
+      color = {
+        ui = true;
+      };
+    };
+    
   };
 }
